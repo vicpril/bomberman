dc39267e7be1f813352b2f89a7d7de2d01203ab4
Viktor Prilepin <vicpride@gmail.com>
2023-03-30 22:53:50 +0700

    Test config added

---
+++ src/frontend/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -0,0 +1,55 @@
+import { AsyncThunkAction } from '@reduxjs/toolkit'
+import axios, { AxiosStatic } from 'axios'
+import { StateSchema } from '@/app/providers/StoreProvider'
+
+type ActionCreatorType<Result, Args, RejectedValue>
+  = (args: Args) => AsyncThunkAction<Result, Args, { rejectValue: RejectedValue }>
+
+jest.mock('axios')
+
+const mockedAxios = jest.mocked(axios)
+
+export class TestAsyncThunk<Result, Args, RejectedValue> {
+  getState: () => StateSchema
+
+  dispatch: jest.MockedFn<any>
+
+  actionCreator: ActionCreatorType<Result, Args, RejectedValue>
+
+  api: jest.MockedFunctionDeep<AxiosStatic>
+
+  navigate: jest.Mock<any>
+
+  constructor(
+    actionCreator: ActionCreatorType<Result, Args, RejectedValue>,
+    responseMock?: object,
+  ) {
+    this.actionCreator = actionCreator
+    this.getState = jest.fn()
+    this.dispatch = jest.fn()
+
+    this.api = mockedAxios
+    this.navigate = jest.fn()
+
+    if (responseMock) {
+      this.api.get.mockResolvedValue(Promise.resolve(responseMock))
+      this.api.post.mockResolvedValue(Promise.resolve(responseMock))
+      this.api.put.mockResolvedValue(Promise.resolve(responseMock))
+      this.api.patch.mockResolvedValue(Promise.resolve(responseMock))
+      this.api.delete.mockResolvedValue(Promise.resolve(responseMock))
+    }
+  }
+
+  async callThunk(args: Args) {
+    const action = this.actionCreator(args)
+
+    const result = await action(
+      // @ts-ignore
+      this.dispatch,
+      this.getState,
+      { api: this.api, navigate: this.navigate },
+    )
+
+    return result
+  }
+}
