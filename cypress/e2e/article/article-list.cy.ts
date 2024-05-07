694d6d53c14f1d0368c6e2a1aca171a25eca4a3b
Viktor Prilepin <vicpride@gmail.com>
2024-05-07 11:34:38 +0700

    e2e: stubs and fixtures

--- cypress/e2e/article/article-list.cy.ts
+++ cypress/e2e/article/article-list.cy.ts
@@ -9,4 +9,16 @@ describe('Пользователь заходит на страницу со с�
     cy.getByTestId('ArticleList').should('exist')
     cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3)
   })
+
+  it('Пример на стабах (фикстурах)', () => {
+    cy.intercept('GET', '/articles?*', { fixture: 'articles.json' })
+    cy.getByTestId('ArticleList').should('exist')
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3)
+  })
+
+  it.skip('Пример пропущенного теста', () => {
+    cy.getByTestId('ArticleList').should('exist')
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3)
+    cy.get('11111').should('exist')
+  })
 })
